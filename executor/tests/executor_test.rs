e9b547dc903f1ce57f5626179abfa9bff54c13aa
Zephyr <180604036@qq.com>
2026-03-26 15:22:44 +0800

    add test case:should_return_y_plus_1_given_command_is_m_and_facing_is_n

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -3,7 +3,7 @@ mod move_tests{
     use super::*;
 
     #[test]
-
+    #[ignore]
     fn should_return_x_plus_1_given_command_is_m_and_facing_is_e(){
 
         let original_pose = Pose::new(0,0,'E');
@@ -13,6 +13,19 @@ mod move_tests{
         executor.execute("M");
 
         let expected_pose = Pose::new(1,0,'E');
+        assert_eq!(expected_pose,executor.query());
+    }
+
+    #[test]
+    fn should_return_y_plus_1_given_command_is_m_and_facing_is_n(){
+
+        let original_pose = Pose::new(0,0,'N');
+
+        let mut executor = Executor::with_pose(original_pose);
+
+        executor.execute("M");
+
+        let expected_pose = Pose::new(0,1,'N');
         assert_eq!(expected_pose,executor.query());
     }
 
