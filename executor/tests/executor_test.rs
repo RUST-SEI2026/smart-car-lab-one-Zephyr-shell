4271c69a076789c6ac0d90dad985d36726efa14c
Zephyr <180604036@qq.com>
2026-03-26 15:26:53 +0800

    add test case:should_return_x_minus_1_given_command_is_m_and_facing_is_w

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -17,6 +17,7 @@ mod move_tests{
     }
 
     #[test]
+    #[ignore]
     fn should_return_y_plus_1_given_command_is_m_and_facing_is_n(){
 
         let original_pose = Pose::new(0,0,'N');
@@ -29,5 +30,17 @@ mod move_tests{
         assert_eq!(expected_pose,executor.query());
     }
 
+    #[test]
+    fn should_return_x_minus_1_given_command_is_m_and_facing_is_w(){
+
+        let original_pose = Pose::new(0,0,'W');
+
+        let mut executor = Executor::with_pose(original_pose);
+
+        executor.execute("M");
+
+        let expected_pose = Pose::new(-1,0,'W');
+        assert_eq!(expected_pose,executor.query());
+    }
 
 }
